439e53ed53f517682874e65a6d6e3d255158a74d
Lérè <lerebear@users.noreply.github.com>
2025-07-11 11:23:40 -0700

    Add .vimrc configuration to setup script

--- setup.sh
+++ setup.sh
@@ -4,3 +4,4 @@ curl -fsSL https://d2lang.com/install.sh | sh -s --
 # Move configuration into place
 cp /workspaces/.codespaces/.persistedshare/dotfiles/.bashrc ~/.bashrc
 cp /workspaces/.codespaces/.persistedshare/dotfiles/.gitconfig ~/.gitconfig
+cp /workspaces/.codespaces/.persistedshare/dotfiles/.vimrc ~/.vimrc
