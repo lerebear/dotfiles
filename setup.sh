cca3e1d7658804c103c0154205f6a9d460142d1a
Lérè <lerebear@users.noreply.github.com>
2025-07-11 11:22:59 -0700

    Copy configuration files into place as part of setup
    ...for .bashrc and .gitconfig from persisted share.

--- setup.sh
+++ setup.sh
@@ -1,2 +1,6 @@
 # Install D2 (declarative diagramming tool)
 curl -fsSL https://d2lang.com/install.sh | sh -s --
+
+# Move configuration into place
+cp /workspaces/.codespaces/.persistedshare/dotfiles/.bashrc ~/.bashrc
+cp /workspaces/.codespaces/.persistedshare/dotfiles/.gitconfig ~/.gitconfig
